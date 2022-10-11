4fc89b288bb3b41c84f8bef490ed7cd709dc7652
Tajinder Singh <tajindr.developer@gmail.com>
2022-10-11 13:10:33 +0530

    dark mode completed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import TextForm from './components/TextForm';
 // import About from './components/About';
 import React, {useState} from 'react';
 
-let name = "Tajinder";
-
 function App() {
   const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#2c2d32';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="Second title" mode={mode}/>
+      <Navbar title="Second title" aboutText='About' mode={mode} toggleMode={toggleMode} />
       <div className="container p-4">
-        <h1>My Name is {name}</h1>
+        <TextForm heading="This is a testing form." mode={mode}/>
       </div>
-      <TextForm heading="This is a testing form."/>
       {/* <About/> */}
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -34,11 +34,11 @@ export default function TextForm(props) {
     //setText("Enter the fucking text here.....");
   return (
     <>
-    <div className="container">
+    <div className={`container text-${props.mode==='light'?'dark':'light'}`}>
         <h1>{props.heading}</h1>
 
         <div className="input-group">
-            <textarea className="form-control" aria-label="With textarea" id='myBox' value={text} onChange={handleOnChange}></textarea>
+            <textarea className="form-control" aria-label="With textarea" id='myBox' style={{backgroundColor: props.mode==='light'?'white':'#2c2d32', color: props.mode==='dark'?'white':'#2c2d32'}} value={text} onChange={handleOnChange}></textarea>
         </div>
         <button className="btn btn-primary my-3" onClick={handleUpClick}>Submit UpperCase</button>
         <button className="btn btn-primary m-3" onClick={handleLoClick}>Submit LowerCase</button>
@@ -46,13 +46,13 @@ export default function TextForm(props) {
         <button className="btn btn-success m-3" onClick={handleCopyClick}>Copy</button>
         <button className="btn btn-primary m-3" onClick={handleExtraSpaces}>Remove Spaces</button>
     </div>
-    <div className="container">
-        <h2>For Results</h2>
+    <div className={`container text-${props.mode==='light'?'dark':'light'}`}>
+        <h2>Your Text Summary</h2>
         <p>There are {text.length} characters in the above field</p>
         <p>There are {text.split(" ").length} Words in the above field</p>
         <p>IT will Take {0.008 * text.split(" ").length} Minutes to read the above data in the input field...</p>
         <h3>Preview:</h3>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something in the textbox to preview.."}</p>
     </div>
     </>
   )
